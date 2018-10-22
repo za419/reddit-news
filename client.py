545ec8055b6ac1179adc88b3855539deef588fea
Ryan Hodin <ryan951357@gmail.com>
2018-10-21 21:27:15 -0500

    Add error handling
    At least now we get prettier errors.

--- client.py
+++ client.py
@@ -3,7 +3,21 @@
 import praw
 import os
 import sys
+import traceback
 from configparser import ConfigParser
+
+# Helper functions
+def exc_message():
+    """
+    Returns a string containing tracebackless exception data.
+    Must only be called within an exception handler.
+    """
+
+    ex_type, ex_value, ex_traceback = sys.exc_info()
+
+    trace=traceback.extract_tb(ex_traceback)[0]
+
+    return "{0}: {1}. From line {2} ({3})".format(ex_type.__name__, ex_value, trace[1], trace[3])
 
 # Get client info from secrets file
 secrets = ConfigParser(interpolation=None)
@@ -21,15 +35,25 @@ reddit=praw.Reddit(user_agent="Comment Fetcher", client_id=secrets['client_id'],
 # Get a submission object
 submission=None
 # The submission is a URL if it includes 'reddit.com', else an ID
-if "reddit.com" in sys.argv[1]:
-    submission=reddit.submission(url=sys.argv[1])
-else:
-    submission=reddit.submission(id=sys.argv[1])
-    
-# Remove 'more comments' and the like
-submission.comments.replace_more(limit=None)
+try:
+    if "reddit.com" in sys.argv[1]:
+        submission=reddit.submission(url=sys.argv[1])
+    else:
+        submission=reddit.submission(id=sys.argv[1])
+except:
+    print("Unable to find reddit submission.\n{0}".format(exc_message()))
+    sys.exit(1)
+
 
 # Now, iterate over all comments, and print them all out
+try:
+    # Remove 'more comments' and the like
+    submission.comments.replace_more(limit=None)
+    all=submission.comments.list()
+except:
+    print("Unable to get comments from submission.\n{0}".format(exc_message()))
+    sys.exit(1)
+
 for comment in submission.comments.list():
     try:
         print(comment.body)
