69b3575b7607a1631e0942f2e9bb2868258d7f60
Ryan Hodin <ryan951357@gmail.com>
2018-10-21 20:46:33 -0500

    Acquire a target submission

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 
 import praw
 import os
+import sys
 from configparser import ConfigParser
 
 # Get client info from secrets file
@@ -9,5 +10,18 @@ secrets = ConfigParser(interpolation=None)
 secrets.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'secrets.ini'))
 secrets=secrets['SECRETS']
 
+# Get target url
+if len(sys.argv)!=2:
+    print("Usage: {0} <target>".format(argv[0]))
+    print("  <target> can either be a link to a Reddit thread, or just a submission ID.")
+
 reddit=praw.Reddit(user_agent="Comment Fetcher", client_id=secrets['client_id'],
                    client_secret=secrets['client_secret'])
+
+# Get a submission object
+submission=None
+# The submission is a URL if it includes 'reddit.com', else an ID
+if "reddit.com" in sys.argv[1]:
+    submission=reddit.submission(url=sys.argv[1])
+else:
+    submission=reddit.submission(id=sys.argv[1])
