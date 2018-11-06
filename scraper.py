da488d1868b9f34f96c1995cd4fc5180a16249fd
JakobFrank <jakobfrank97@gmail.com>
2018-11-05 20:11:47 -0600

    Scraper prints using utf-8

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from readability.readability import Document
 import urllib.request
 
+sys.stdout.reconfigure(encoding='utf-8')
 html = urllib.request.urlopen('https://www.theguardian.com/us-news/2018/nov/04/pittsburgh-shooting-robert-bowers-jewish-nurse').read()
 doc = Document(html)
 doc.parse(["summary", "short_title"])
