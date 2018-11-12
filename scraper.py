c4a19367e8534b2426e26c4d5df2ae759facf3e5
Jakob Frank <jakobfrank97@gmail.com>
2018-11-11 21:51:32 -0600

    Turned scraper into scrape function

--- scraper.py
+++ scraper.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 from readability.readability import Document
 import urllib.request
 
-sys.stdout.reconfigure(encoding='utf-8')
-html = urllib.request.urlopen('https://www.theguardian.com/us-news/2018/nov/04/pittsburgh-shooting-robert-bowers-jewish-nurse').read()
-doc = Document(html)
-doc.parse(["summary", "short_title"])
-readable_article = doc.summary()
-readable_title = doc.short_title()
-soup = BeautifulSoup(readable_article, 'html.parser')
-text = soup.get_text()
-print(text)
+def scrape(URL):
+    sys.stdout.reconfigure(encoding='utf-8')
+    html = urllib.request.urlopen(URL).read()
+    doc = Document(html)
+    doc.parse(["summary", "short_title"])
+    readable_article = doc.summary()
+    soup = BeautifulSoup(readable_article, 'html.parser')
+    text = soup.get_text()
+    return text
