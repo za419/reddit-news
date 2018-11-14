ca2c22360055be24e198911836b34e5d8a6425db
Ryan Hodin <ryan951357@gmail.com>
2018-11-13 22:09:06 -0600

    Allow scraper to be called by itself
    That is, scraper is now both a commandline tool and it's own library

--- scraper.py
+++ scraper.py
@@ -11,3 +11,21 @@ def scrape(URL):
     soup = BeautifulSoup(readable_article, 'html.parser')
     text = soup.get_text()
     return text
+
+if __name__ == "__main__":
+    # Get target url
+    if len(sys.argv)!=2:
+        try:
+            print("Usage: python {0} <url>".format(sys.argv[0]))
+        except:
+            print("Usage: python scraper.py <url>")
+        print("  <url> should point to an article from which to scrape text.")
+        sys.exit(1)
+
+    # Under Python 3.7, use utf-8 output
+    try:
+        sys.stdout.reconfigure(encoding='utf-8')
+    except:
+        pass
+
+    print(scrape(sys.argv[1]))
