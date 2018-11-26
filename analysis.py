8e5d4647e73aae1da48b3308693715d8d4dbdb3d
Ryan Hodin <ryan951357@gmail.com>
2018-11-25 21:16:02 -0600

    Add docstring to analyze

--- analysis.py
+++ analysis.py
@@ -6,6 +6,15 @@ nlp.vocab["\n"].is_stop = True
 nlp.vocab["'s"].is_stop = True
 
 def analyze(a, b):
+    """
+    Perform text analysis on passed article text and list of comment triples (from client.py)
+    Returns a pair of keyword lists.
+    The first list is those which are common between the comments and the article.
+    The second is those which only appear in the comments.
+
+    Each keyword list is itself a list of quads: (commentID, commentPermalink, keyword, frequency)
+    commentID and commentPermalink point to some comment which referred to the keyword.
+    """
 
     doc1 = nlp(a)
     arr1 = []
